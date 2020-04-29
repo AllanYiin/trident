43fe4b9e2c6735512fc0a252b920b43a131bf764
Allan <allan@asiaminer.com.tw>
2020-04-29 13:22:30 +0800

    change import

--- trident/__init__.py
+++ trident/__init__.py
@@ -11,14 +11,17 @@ if sys.getdefaultencoding() != defaultencoding:
 
 
 
-__version__ = '0.5.1'
+__version__ = '0.5.2'
 stderr.write('trident {0}\n'.format(__version__))
-from .backend import *
-from trident import models
-from trident import misc
-from trident import callbacks
-from trident import data
 
+
+
+from . import backend
+from .backend import *
+from . import models
+from . import misc
+from . import callbacks
+from . import data
 import threading
 
 
