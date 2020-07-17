86fbcaf475ccc6384d8302f2e9543b25e771b6c6
Allan <allan@asiaminer.com.tw>
2020-07-17 17:24:32 +0800

    add AverageMeter

--- trident/backend/common.py
+++ trident/backend/common.py
@@ -433,7 +433,23 @@ def get_signature(fn,name=None):
     return signature
 
 
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
 
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
 
 
 
