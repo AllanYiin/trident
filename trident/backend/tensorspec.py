a8782816bbfcb4bcaf867955927bdc44ed932886
Allan <allan@asiaminer.com.tw>
2020-12-29 12:18:01 +0800

    new class method tensor_to_spec

--- trident/backend/tensorspec.py
+++ trident/backend/tensorspec.py
@@ -134,8 +134,8 @@ class TensorSpec(object):
                 raise ValueError('Axis {} is greater than the maximum allowed value: {}'
                                  .format(max_axis, max_dim))
     @classmethod
-    def tensor_to_spec(cls, t:Tensor):
-        return cls(shape=tensor_to_shape(t),dtype=t.dtype,name=t.name)
+    def tensor_to_spec(cls, t:Tensor, object_type:ObjectType=None):
+        return cls(shape=tensor_to_shape(t),dtype=t.dtype,object_type=object_type,name=t.name)
 
 
     @property
