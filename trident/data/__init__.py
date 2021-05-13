f82ca6c9cffe50f490b825b752a2029e2add107d
Allan <allan@asiaminer.com.tw>
2021-05-13 16:10:23 +0800

    rearrange imports

--- trident/data/__init__.py
+++ trident/data/__init__.py
@@ -4,9 +4,6 @@ from __future__ import print_function
 
 import os
 import threading
-from trident.data.transform import *
-from trident.data.vision_transforms import *
-
 
 from trident.data.image_common import *
 
@@ -23,6 +20,11 @@ from . import label_common
 from . import mask_common
 from . import bbox_common
 from . import text_common
+
+from trident.data.transform import *
+from trident.data.vision_transforms import *
+from trident.data.text_transforms import *
+
 
 
 from trident.data.image_reader import ImageReader,ImageThread
