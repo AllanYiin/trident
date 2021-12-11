bugfix:get nan when calculate forward or loss in automatic mixed precision training

trident/optims/losses.py
@@ -5,15 +5,20 @@ from __future__ import print_function
 import sys
 from typing import Callable, Any
 
+import numpy as np
+import torch
+
 from trident.backend.common import get_session, OrderedDict
 from trident.backend.tensorspec import *
 
 _session = get_session()
 _backend = _session.backend
 if _backend == 'pytorch':
+    import torch
     from trident.backend.pytorch_ops import *
 
 elif _backend == 'tensorflow':
+    import tensorflow as tf
     from trident.backend.tensorflow_ops import *
 
 __all__ = ['Loss']
@@ -114,10 +119,10 @@ class Loss(object):
             return loss
         if ndim(loss) >= 2 and self.reduction == 'batch_sum':
             loss = reshape(loss, (int_shape(loss)[0], -1))
-            return loss.sum(1).mean()
+            return loss.sum(0).mean()
         elif ndim(loss) >= 2 and self.reduction == 'batch_mean':
             loss = reshape(loss, (int_shape(loss)[0], -1))
-            return loss.mean(1).mean()
+            return loss.mean(0).mean()
         elif self.reduction in ('mean', 'batch_mean'):
             return loss.mean()
         elif self.reduction in ('sum', 'batch_sum'):
@@ -167,4 +172,52 @@ class Loss(object):
            self._signature.outputs=new_output
 
 
+def _check_logsoftmax_logit(x:Tensor,axis=1):
+    if isinstance(x, np.ndarray):
+        if _backend == 'pytorch':
+            if axis is None:
+                axis = 1
+        elif _backend == 'tensorflow':
+            if axis is None:
+                axis = -1
+        if reduce_max(x) <= 0:
+            output_exp = exp(x)
+            return abs(1-reduce_mean(output_exp.sum(axis=axis)))<0.05
+        return False
+    elif _backend == 'pytorch':
+        with torch.no_grad():
+            if reduce_max(x)<=0:
+                output_exp=exp(x)
+                return abs(1-reduce_mean(output_exp.sum(axis=axis)))<0.05
+            return False
+    elif _backend == 'tensorflow':
+        if reduce_max(x)<=0:
+            output_exp=exp(x).copy().detach()
+            return abs(1-reduce_mean(output_exp.sum(axis=axis)))<0.05
+        return False
+
+def _check_logit(x:Tensor,axis=None):
+    if isinstance(x,np.ndarray):
+        if _backend == 'pytorch':
+            if axis is None:
+                axis = 1
+        elif _backend == 'tensorflow':
+            if axis is None:
+                axis = -1
+        if reduce_max(x) <= 0:
+            return abs(1 - reduce_mean(x.sum(axis=axis))) < 0.05
+        return False
+    elif _backend == 'pytorch':
+        if axis is None:
+            axis=1
+        with torch.no_grad():
+            if reduce_max(x)<=0:
+                return abs(1-reduce_mean(x.sum(axis=axis)))<0.05
+            return False
+    elif _backend == 'tensorflow':
+        if axis is None:
+            axis = -1
+        if reduce_max(x)<=0:
+            return abs(1-reduce_mean(x.sum(axis=axis)))<0.05
+        return False
 

trident/optims/pytorch_losses.py
@@ -606,7 +606,7 @@ class CrossEntropyLoss(_ClassificationLoss):
         tensor(1.4518)
         """
 
-        sample_weight = cast(self.sample_weight, output.dtype) * cast(self.ignore_index_weight, output.dtype)
+        sample_weight = (cast(self.sample_weight, output.dtype) * cast(self.ignore_index_weight, output.dtype)).detach()
 
 
 
@@ -625,7 +625,7 @@ class CrossEntropyLoss(_ClassificationLoss):
             #print(10, 'output', output.shape, 'abnormal:', any_abnormal_number(output))
             if not self.is_logsoftmax and reduce_min(output)>=0:
                 output=clip(log_softmax(output, axis=1), min=-12, max=-1e-7)
-
+            target = target.detach()
             return nn.functional.nll_loss(output,target,sample_weight,reduction='none')+gather_unbalance_weight
         else:
             unbalance_weight=ones(tuple([1]*(ndim(output)-1)+[ self.num_classes]))
@@ -636,6 +636,7 @@ class CrossEntropyLoss(_ClassificationLoss):
             if not self.is_logsoftmax and reduce_min(output)>=0:
                 output=clip(log_softmax(output, axis=1), min=-12, max=-1e-7)
             sample_weight = expand_as(sample_weight, output)
+            target=target.detach()
             return -reduce_sum(target * output * sample_weight*unbalance_weight, axis=1)
 
 
@@ -1089,11 +1090,11 @@ class DiceLoss(_ClassificationLoss):
         #     print('{0} calculate starting'.format(self.name))
         if self.is_logsoftmax:
             output =clip(exp(clip(output,max=0)),1e-7,1-1e-7)
-
+        #
         # if any_abnormal_number(output):
         #     print('{0} after exp'.format(self.name))
-        #print(11, 'output_exp', output_exp.shape, 'abnormal:', any_abnormal_number(output_exp))
-        sample_weight = self.sample_weight.to(get_device()) * self.ignore_index_weight.to(get_device())
+        target=target.detach()
+        sample_weight = (self.sample_weight.to(get_device()) * self.ignore_index_weight.to(get_device())).detach()
 
         intersection = reduce_sum(target * output, axis=-1)
         # if any_abnormal_number(intersection):
@@ -1106,10 +1107,16 @@ class DiceLoss(_ClassificationLoss):
         #     print('{0} after den2'.format(self.name))
 
         sample_weight = expand_as(sample_weight, intersection).detach()
-        dice = 1.0 -( (2.0 * (intersection*sample_weight).sum(1) + self.smooth) / (den1.sum(1)  + den2.sum(1)  + self.smooth)).mean()
+
+        if output.shape[1]==2:
+            dice = (1.0 - (2.0 * (intersection * sample_weight)[:,1:].sum(1) + self.smooth) / (den1[:,1:].sum(1) + den2[:,1:].sum(1) + self.smooth))
+        else:
+
+            dice = 1.0 -  torch.div((2.0 * (intersection * sample_weight).sum(1) + self.smooth) , (den1.sum(1) + den2.sum(1) + self.smooth))
+
         # if any_abnormal_number(den2):
         #     print('{0} after dice {1}'.format(self.name,dice))
-        #print(12, 'dice', dice.shape, 'abnormal:', any_abnormal_number(dice))
+
         return dice
 
 
@@ -1544,30 +1551,25 @@ class IoULoss(_ClassificationLoss):
     def __init__(self, axis=1, sample_weight=None, auto_balance=False, from_logits=False, ignore_index=-100, cutoff=None, label_smooth=False, reduction='mean', enable_ohem=False,
                  ohem_ratio=3.5,  input_names=None, output_names=None,name='lou_loss'):
         super(IoULoss, self).__init__(raxis=axis, sample_weight=sample_weight, auto_balance=auto_balance, from_logits=from_logits, ignore_index=ignore_index, cutoff=cutoff,
-                                      label_smooth=label_smooth, reduction=reduction, enable_ohem=enable_ohem, ohem_ratio=ohem_ratio, name=name)
-        self.need_target_onehot = False
+                                      label_smooth=label_smooth, reduction=reduction, enable_ohem=enable_ohem, ohem_ratio=ohem_ratio,input_names=input_names ,output_names=output_names,name=name)
+        self.need_target_onehot = True
         self.is_multiselection = False
         self.is_logsoftmax = False
         self._built = True
 
     def calculate_loss(self, output, target, **kwargs):
-        _dtype = output.dtype
         if self.is_logsoftmax:
-            output = clip(exp(output), 1e-8, 1 - 1e-8)
-            self.from_logits = True
-        if not self.from_logits:
-            output = softmax(output, self.axis)
-        if target.dtype != Dtype.int64 or self.is_target_onehot == True:
-            target = argmax(target, axis=self.axis)
-        batch_size = int_shape(output)[0]
-        output = argmax(output, axis=self.axis)
+            output =clip(exp(clip(output,max=0)),1e-7,1-1e-7)
 
-        output_flat = output.reshape((batch_size, -1))
-        target_flat = target.reshape((batch_size, -1))
-        intersection = equal(output_flat, target_flat, dtype=_dtype).sum()
-        union = greater(greater(output_flat, 0, dtype=_dtype) + greater(target_flat, 0, dtype=_dtype), 0, dtype=_dtype).sum().clamp(min=1)
-        loss = 1 - (intersection / union)
-        return loss
+        sample_weight = (self.sample_weight.to(get_device()) * self.ignore_index_weight.to(get_device()))
+        target=target.detach()
+        sample_weight = expand_as(sample_weight, output).detach()
+        intersection = reduce_sum(target * output*sample_weight, axis=-1)
+        union = reduce_sum(output*sample_weight, axis=-1)+reduce_sum(target, axis=-1)-intersection
+
+
+        iou=( intersection).sum(1) / (union + 1e-7).sum(1)
+        return (1-iou).mean()
 
 
 class SoftIoULoss(Loss):

trident/optims/pytorch_metrics.py
@@ -15,7 +15,7 @@ from torch.autograd import Variable
 from trident.backend.common import get_session, addindent, get_time_suffix, get_class, get_function, camel2snake
 from trident.backend.pytorch_ops import *
 from trident.data.mask_common import mask2trimap
-
+from trident.optims.losses import _check_logsoftmax_logit
 __all__ = ['accuracy','recall','pixel_accuracy','alpha_pixel_accuracy','iou','psnr','mean_absolute_error','mean_squared_error','mean_squared_logarithmic_error','mae','mse','rmse','msle','get_metric']
 
 # def accuracy(input, target,axis=1):
@@ -53,12 +53,12 @@ def accuracy(output:Tensor, target:Tensor, topk:int=1,axis:int=1,ignore_index:Un
     if (ndim(output_tensor) >= 1 and 'float' in str(output_tensor.dtype) and output_tensor.min() >= 0 and output_tensor.max() <= 1):
         is_logsoftmax = False
         from_logits = True
-        output_tensor = clip(output_tensor, min=1e-8, max=1 - 1e-8)
+        output_tensor = clip(output_tensor, min=1e-7, max=1 - 1e-7)
 
     elif (ndim(output_exp) >= 1 and 'float' in str(output_exp.dtype) and output_exp.min() >= 0 and output_exp.max() <= 1):
         is_logsoftmax = True
         from_logits = True
-        output_tensor =  clip(output_exp, min=1e-8, max=1 - 1e-8)
+        output_tensor =  clip(output_exp, min=1e-7, max=1 - 1e-7)
     else:
         is_logsoftmax = False
         from_logits = False
@@ -109,12 +109,12 @@ def recall(output:Tensor, target:Tensor, axis=1,ignore_index=0):
     if (ndim(output_tensor) >= 1 and 'float' in str(output_tensor.dtype) and output_tensor.min() >= 0 and output_tensor.max() <= 1):
         is_logsoftmax = False
         from_logits = True
-        output_tensor = clip(output_tensor, min=1e-8, max=1 - 1e-8)
+        output_tensor = clip(output_tensor, min=1e-7, max=1 - 1e-7)
 
     elif (ndim(output_exp) >= 1 and 'float' in str(output_exp.dtype) and output_exp.min() >= 0 and output_exp.max() <= 1):
         is_logsoftmax = True
         from_logits = True
-        output_tensor =  clip(output_exp, min=1e-8, max=1 - 1e-8)
+        output_tensor =  clip(output_exp, min=1e-7, max=1 - 1e-7)
     else:
         is_logsoftmax = False
         from_logits = False
@@ -248,11 +248,18 @@ def alpha_pixel_accuracy(output, alpha):
 
 @torch.no_grad()
 def iou(output:Tensor, target:Tensor):
-    output_tensor = output.clone().detach()
-    target_tensor = target.clone().detach()
+    output_tensor = output.clone().detach().view(output.size(0),output.size(1),-1)
+    target_tensor=None
+    if ndim(output)==ndim(target)+1:
+        target_tensor = target.clone().detach().view(target.size(0),-1)
+    elif ndim(output)==ndim(target):
+        target_tensor = target.clone().detach().view(target.size(0),target.size(1),-1)
+        target_tensor=argmax(target_tensor,1)
+
+    if _check_logsoftmax_logit(output_tensor):
+        output_tensor=exp(output_tensor)
     if output_tensor.dtype != torch.int64:
         output_tensor = argmax(output_tensor, axis=1).squeeze()
-
     intersection =( (output_tensor > 0) * (output_tensor == target_tensor)).sum().float()
     union=((output_tensor+target_tensor)>0).sum().float()
 

trident/optims/pytorch_optimizers.py
@@ -241,8 +241,8 @@ class Adam(Optimizer):
 
                 G_grad = exp_avg / denom
                 if self.gradient_centralization in ['all', 'gc']:
-                    if len(list(G_grad.size())) > 1:
-                        G_grad.add_(-G_grad.mean(dim=tuple(range(1, len(list(G_grad.size())))), keepdim=True))
+                    if ndim(G_grad) > 1:
+                        G_grad.add_(-G_grad.mean(axis=list(range(1, ndim(G_grad))), keepdims=True))
 
                 p.data.add_(G_grad, alpha=-step_size)
                 if half_precision:
@@ -1411,18 +1411,20 @@ class RangerLars(Optimizer):
                 update = zeros_like(p_data)
                 if N_sma >= 5:
                     denom = exp_avg_sq.sqrt().add_(group['eps'])
-                    update.addcdiv_(exp_avg, denom, value=step_size)
+
+                    G_grad = (exp_avg/denom) * step_size
                 else:
-                    update.add_(exp_avg, alpha=step_size)
+                    G_grad=exp_avg*step_size
+
 
                 # if group['weight_decay'] != 0:
                 #     update.add_(group['weight_decay'], p_data)
                 if group['weight_decay'] != 0:
-                    grad = grad.add(p, alpha=group['weight_decay'])
+                    G_grad = G_grad.add(p, alpha=group['weight_decay'])
 
                 if self.gradient_centralization in ['all', 'gc']:
-                    if len(list(grad.size())) > 1:
-                        grad.add_(-grad.mean(dim=tuple(range(1, len(list(grad.size())))), keepdim=True))
+                    if ndim(G_grad)> 1:
+                        G_grad=G_grad-G_grad.mean(axis=list(range(1, ndim(G_grad))), keepdims=True)
 
                 radam_norm = update.pow(2.0).sum().sqrt()
                 weight_norm = p.data.pow(2.0).sum().sqrt()
@@ -1655,7 +1657,7 @@ class AdaBelief(Optimizer):
     reference: AdaBelief Optimizer, adapting stepsizes by the belief in observed gradients, NeurIPS 2020
     """
 
-    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-16,
+    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-7,
                  weight_decay=0, amsgrad=False, weight_decouple=True, fixed_decay=False, rectify=True,
                  degenerated_to_sgd=True, gradient_centralization=None,**kwargs):
         if not 0.0 <= lr:
@@ -1769,8 +1771,8 @@ class AdaBelief(Optimizer):
                 exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
 
                 if self.gradient_centralization in ['all', 'gcc']:
-                    if len(list(grad.size())) > 3:
-                        grad.add_(-grad.mean(dim=tuple(range(1, grad.dim())), keepdim=True))
+                    if ndim(grad)> 3:
+                        grad=grad-grad.mean(axis=list(range(1, ndim(grad))), keepdims=True)
 
                 state['step'] += 1
                 bias_correction1 = 1 - beta1 ** state['step']
@@ -1829,8 +1831,9 @@ class AdaBelief(Optimizer):
 
 
                     if self.gradient_centralization in ['all', 'gc']:
-                        if len(list(G_grad.size())) > 1:
-                            G_grad.add_(-G_grad.mean(dim=tuple(range(1, len(list(G_grad.size())))), keepdim=True))
+                        if ndim(G_grad) > 1:
+                            G_grad = G_grad - G_grad.mean(axis=list(range(1, ndim(G_grad))), keepdims=True)
+
 
                     if any_abnormal_number(p_data_fp32):
                         sys.stderr.write('{0} p_data has abnormal value,trident automatically replace these abnormal value to zero.\n\r'.format(self.__class__.__name__))
@@ -1993,8 +1996,8 @@ class RangerAdaBelief(Optimizer):
                 # if grad.dim() > self.gc_gradient_threshold:
                 #    grad.add_(-grad.mean(dim=tuple(range(1, grad.dim())), keepdim=True))
                 if self.gradient_centralization in ['all', 'gcc']:
-                    if len(list(grad.size())) > 3:
-                        grad.add_(-grad.mean(dim=tuple(range(1, grad.dim())), keepdim=True))
+                    if ndim(grad)> 3:
+                        grad=grad-grad.mean(axis=list(range(1, ndim(grad))), keepdims=True)
 
                 state['step'] += 1
 
@@ -2046,8 +2049,8 @@ class RangerAdaBelief(Optimizer):
                 # GC operation
 
                 if self.gradient_centralization in ['all', 'gc']:
-                    if len(list(G_grad.size())) > 1:
-                        G_grad.add_(-G_grad.mean(dim=tuple(range(1, len(list(G_grad.size())))), keepdim=True))
+                    if ndim(G_grad)> 1:
+                        G_grad=G_grad-G_grad.mean(axis=list(range(1, ndim(G_grad))), keepdims=True)
 
 
                 p_data_fp32.add_(G_grad, alpha=-step_size * group['lr'])
@@ -2092,7 +2095,7 @@ class DiffGrad(Optimizer):
         https://openreview.net/forum?id=ryQu7f-RZ
     """
 
-    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8, weight_decay=0, gradient_centralization=None):
+    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-7, weight_decay=0, gradient_centralization=None):
         if not 0.0 <= lr:
             raise ValueError("Invalid learning rate: {}".format(lr))
         if not 0.0 <= eps:
@@ -2153,8 +2156,9 @@ class DiffGrad(Optimizer):
 
 
                 if self.gradient_centralization in ['all', 'gcc']:
-                    if len(list(grad.size())) > 3:
-                        grad.add_(-grad.mean(dim=tuple(range(1, grad.dim())), keepdim=True))
+                    if ndim(grad)> 3:
+                        grad=grad-grad.mean(axis=list(range(1, ndim(grad))), keepdims=True)
+
                 state['step'] += 1
 
                 if group['weight_decay'] != 0:
@@ -2259,8 +2263,8 @@ class Lamb(Optimizer):
                 exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
                 beta1, beta2 = group['betas']
                 if self.gradient_centralization in ['all', 'gcc']:
-                    if len(list(grad.size())) > 3:
-                        grad.add_(-grad.mean(dim=tuple(range(1, grad.dim())), keepdim=True))
+                    if ndim(grad)> 3:
+                        grad=grad-grad.mean(axis=list(range(1, ndim(grad))), keepdims=True)
 
                 state['step'] += 1
 

trident/optims/pytorch_trainer.py
@@ -40,7 +40,6 @@ from trident.backend.pytorch_ops import *
 from trident.backend import model
 from trident.data.mask_common import color2label
 from trident.data.transform import Transform
-from trident.callbacks import UnfreezeModelCallback, LambdaCallback
 from trident.backend.opencv_backend import array2image, image2array
 
 from trident.data.dataset import Iterator, NumpyDataset, LabelDataset
@@ -347,41 +346,42 @@ class Model(model.ModelBase):
         else:
             return []
 
-    def complie(self, optimizer="Adam",
+    def complie(self, optimizer="AdaBelief",
                 loss=None,
                 metrics=None,
                 loss_weights=None,
                 **kwargs
                 ):
         self.with_optimizer(optimizer, lr=2e-3, betas=(0.9, 0.999))
-        if loss is not None:
-            if isinstance(loss, str) or callable(loss) or inspect.isfunction(loss) or inspect.isclass(loss):
-                loss_weights = 1.0 if loss_weights is None or not isinstance(loss, numbers.Number) else loss_weights
-                self.with_loss(loss, loss_weight=loss_weights)
-            elif isinstance(loss, list):
-                if loss_weights is not None and isinstance(loss_weights, list) and len(loss_weights) == len(loss):
-                    for k in range(len(loss)):
-                        loss_item = loss[k]
-                        weight = loss_weights[k] if isinstance(loss_weights[k], numbers.Number) else 1.0
-                        self.with_loss(loss_item, loss_weight=weight)
-                else:
-                    for loss_item in loss:
-                        self.with_loss(loss_item)
+        if loss is not None and (isinstance(loss, str) or callable(loss) or inspect.isfunction(loss) or inspect.isclass(loss)):
+            self.with_loss(loss, loss_weight=loss_weights)
+        else:
+            if loss is None:
+                loss = [CrossEntropyLoss]
+            elif not  isinstance(loss, (tuple, list, dict) ):
+                loss=[loss]
+
+            if loss_weights is None :
+                loss_weights=[1]*len(loss)
+            elif isinstance(loss_weights,numbers.Number):
+                loss_weights = [loss_weights] * len(loss)
+            elif len(loss_weights)!=len(loss):
+                loss_weights = [1] * len(loss)
+
+            if isinstance(loss,  (list, tuple) ):
+                for l,w in zip(loss,loss_weights):
+                    self.with_loss(l, loss_weight=w)
 
             elif isinstance(loss, dict):
-                if loss_weights is not None and isinstance(loss_weights, dict):
-                    for k, v in loss.items():
-                        if k in loss_weights:
-                            weight = loss_weights[k] if isinstance(loss_weights[k], numbers.Number) else 1.0
-                            self.with_loss(v, loss_weight=weight, name=k)
-                        else:
-                            self.with_loss(v, loss_weight=1.0, name=k)
-                else:
-                    for k, v in loss.items():
-                        self.with_loss(v, loss_weight=1., name=k)
-        if metrics is not None:
-            if isinstance(metrics, str) or callable(metrics) or inspect.isfunction(metrics) or inspect.isclass(metrics):
-                self.with_metric(metrics)
+                for k,v,w in zip(loss.items(),loss_weights):
+                    self.with_loss(v, loss_weight=w,name=k)
+
+
+        if metrics is not None and (isinstance(metrics, str) or callable(metrics) or inspect.isfunction(metrics) or inspect.isclass(metrics)):
+            self.with_metric(metrics)
+        else:
+            if metrics is None:
+                pass
             elif isinstance(metrics, list):
                 for metric in metrics:
                     self.with_metric(metric)
@@ -1044,56 +1044,57 @@ class Model(model.ModelBase):
                 self._model.eval()
                 self._model.cpu()
 
-                temfolder = tempfile.gettempdir()
-                tempfilename = filename + '_' + str(uuid.uuid4().node)
-                temppath = os.path.join(temfolder, tempfilename + ext)
+                #temfolder = tempfile.gettempdir()
+                with tempfile.TemporaryDirectory() as temfolder:
+                    tempfilename = filename + '_' + str(uuid.uuid4().node)
+                    temppath = os.path.join(temfolder, tempfilename + ext)
 
-                try:
-                    with open(temppath, 'wb') as f:
-                        torch.save({
-                            'state_dict': self._model.state_dict(),
-                            'backend': 'pytorch',
-                            'trident_version': __version__,
-                            'pytorch_version': torch.__version__,
-                            'signature': self._model.signature
-                        }, f)
+                    try:
+                        with open(temppath, 'wb') as f:
+                            torch.save({
+                                'state_dict': self._model.state_dict(),
+                                'backend': 'pytorch',
+                                'trident_version': __version__,
+                                'pytorch_version': torch.__version__,
+                                'signature': self._model.signature
+                            }, f)
 
-                    if os.path.exists(save_path):
-                        os.remove(save_path)
-                        shutil.move(temppath, save_path)
-                        #os.rename(move_path, save_path)
-                    else:
-                        shutil.move(temppath, save_path)
-
-                except Exception as e:
-                    ctx.print(e)
-                    if os.path.exists(temppath):
                         if os.path.exists(save_path):
-                            shutil.move(save_path, save_path+'._')
-                        shutil.move(temppath, save_path)
+                            os.remove(save_path)
+                            shutil.move(temppath, save_path)
+                            #os.rename(move_path, save_path)
+                        else:
+                            shutil.move(temppath, save_path)
+
+                    except Exception as e:
+                        ctx.print(e)
+                        if os.path.exists(temppath):
+                            if os.path.exists(save_path):
+                                shutil.move(save_path, save_path+'._')
+                            shutil.move(temppath, save_path)
 
 
-                ext = '.pth'
-                save_path = save_path.replace('.pth.tar', '.pth')
-                tempfilename2 = filename + '_' + str(uuid.uuid4().node)
-                temppath2 = os.path.join(temfolder, tempfilename2 + ext)
+                    ext = '.pth'
+                    save_path = save_path.replace('.pth.tar', '.pth')
+                    tempfilename2 = filename + '_' + str(uuid.uuid4().node)
+                    temppath2 = os.path.join(temfolder, tempfilename2 + ext)
 
-                try:
-                    with open(temppath2, 'wb') as f:
-                        save(self._model, f)
+                    try:
+                        with open(temppath2, 'wb') as f:
+                            save(self._model, f)
 
-                    if os.path.exists(save_path):
-                        os.remove(save_path)
-                        shutil.move(temppath2, save_path)
-                        #os.rename(move_path2, save_path)
-                    else:
-                        shutil.move(temppath2, save_path)
-                except Exception as e:
-                    ctx.print(e)
-                    if os.path.exists(temppath2):
                         if os.path.exists(save_path):
-                            shutil.move(save_path, save_path+'._')
-                        shutil.move(temppath2, save_path)
+                            os.remove(save_path)
+                            shutil.move(temppath2, save_path)
+                            #os.rename(move_path2, save_path)
+                        else:
+                            shutil.move(temppath2, save_path)
+                    except Exception as e:
+                        ctx.print(e)
+                        if os.path.exists(temppath2):
+                            if os.path.exists(save_path):
+                                shutil.move(save_path, save_path+'._')
+                            shutil.move(temppath2, save_path)
 
                 self._model.to(get_device())
                 self._model.train()
@@ -1180,37 +1181,38 @@ class Model(model.ModelBase):
             #     dynamic_axes[inp] = [0]
             # for out in self.outputs.key_list:
             #     dynamic_axes[out] = [0]
-            temfolder = tempfile.gettempdir()
-            tempfilename = filename + '_' + str(uuid.uuid4().node)
-            temppath = os.path.join(temfolder, tempfilename + ext)
+            #temfolder = tempfile.gettempdir()
+            with tempfile.TemporaryDirectory() as temfolder:
+                tempfilename = filename + '_' + str(uuid.uuid4().node)
+                temppath = os.path.join(temfolder, tempfilename + ext)
 
-            try:
-                ctx.print('Export to onnx file starting!')
-                torch.onnx.export(self._model,  # model being run
-                                  dummy_input,  # model input (or a tuple for multiple inputs)
-                                  f=temppath,  # where to save the model (can be a file or file-like object)
-                                  input_names=self.signature.inputs.key_list,  # the model's input names
-                                  output_names=self.signature.outputs.key_list,  # the model's output names
-                                  # _retain_param_name=True,  # store the trained parameter weights inside the model file
-                                  # enable_onnx_checker=True,
-                                  opset_version=9  # the ONNX version to export the model to
+                try:
+                    ctx.print('Export to onnx file starting!')
+                    torch.onnx.export(self._model,  # model being run
+                                      dummy_input,  # model input (or a tuple for multiple inputs)
+                                      f=temppath,  # where to save the model (can be a file or file-like object)
+                                      input_names=self.signature.inputs.key_list,  # the model's input names
+                                      output_names=self.signature.outputs.key_list,  # the model's output names
+                                      # _retain_param_name=True,  # store the trained parameter weights inside the model file
+                                      # enable_onnx_checker=True,
+                                      opset_version=11  # the ONNX version to export the model to
 
-                                  )
-                ctx.print('Export to onnx file finished! !')
+                                      )
+                    ctx.print('Export to onnx file finished! !')
 
-                if os.path.exists(save_path):
-                    os.remove(save_path)
-                    shutil.move(temppath, save_path)
-                else:
-                    shutil.move(temppath, save_path)
-                self._model.train()
-
-            except Exception as e:
-                ctx.print(e)
-                if os.path.exists(temppath):
                     if os.path.exists(save_path):
-                        shutil.move(save_path, save_path + '._')
-                    shutil.move(temppath, save_path)
+                        os.remove(save_path)
+                        shutil.move(temppath, save_path)
+                    else:
+                        shutil.move(temppath, save_path)
+                    self._model.train()
+
+                except Exception as e:
+                    ctx.print(e)
+                    if os.path.exists(temppath):
+                        if os.path.exists(save_path):
+                            shutil.move(save_path, save_path + '._')
+                        shutil.move(temppath, save_path)
 
             import onnx
             from onnx import shape_inference